e59fcea44f25d2d2112ad072e5f7d89aa83f4013
viniciusbgr <vini25013@gmail.com>
2024-08-30 19:25:30 -0400

    fix indent

--- src/events/external/voice-update.ts
+++ src/events/external/voice-update.ts
@@ -2,35 +2,35 @@ import { Bot, ClientEvent } from "../../structures/client";
 import { VoiceState } from "discord.js";
 
 export default class implements ClientEvent {
-  name = "voiceStateUpdate";
+    name = "voiceStateUpdate";
 
-  constructor(private client: Bot) {}
+    constructor(private client: Bot) { }
 
-  run(oldState: VoiceState, newState: VoiceState) {
-    if (oldState.member?.user.bot || newState.member?.user.bot) return;
+    run(oldState: VoiceState, newState: VoiceState) {
+        if (oldState.member?.user.bot || newState.member?.user.bot) return;
 
-    if (oldState.channelId && !newState.channelId) {
-      this.client.manager.players.get(oldState.channelId)?.destroy();
+        if (oldState.channelId && !newState.channelId) {
+            this.client.manager.players.get(oldState.channelId)?.destroy();
 
-      return;
+            return;
+        }
+
+        const voiceChannel = newState.channel;
+
+        if (!voiceChannel) return;
+
+        const player = this.client.manager.players.get(voiceChannel.id);
+
+        if (!player) return;
+
+        if (!voiceChannel.members.filter(member => !member.user.bot).size) {
+            player.destroy(true);
+
+            return
+        }
+
+        if (voiceChannel.id !== player.voiceChannel) {
+            player.setVoiceChannel(voiceChannel.id)
+        }
     }
-
-    const voiceChannel = newState.channel;
-
-    if (!voiceChannel) return;
-
-    const player = this.client.manager.players.get(voiceChannel.id);
-
-    if (!player) return;
-
-    if (!voiceChannel.members.filter(member => !member.user.bot).size) {
-      player.destroy(true);
-
-      return
-    }
-
-    if (voiceChannel.id !== player.voiceChannel) {
-      player.setVoiceChannel(voiceChannel.id)
-    }
-  }
 }
--- src/events/lavalink/node-connect.ts
+++ src/events/lavalink/node-connect.ts
@@ -4,9 +4,9 @@ import { Bot } from "../../structures/client";
 export default class {
     name = "nodeConnect";
 
-    constructor(private _: Bot) {};
+    constructor(private _: Bot) { };
 
-    run(node: Node) : void {
+    run(node: Node): void {
         console.log(`Node ${node.options?.identifier ?? "unknow"} connected`);
     }
 }
